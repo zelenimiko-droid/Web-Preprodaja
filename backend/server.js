12ce40c22b8e8e70f69f1f1eda5980e1af63ea1a
zelenimiko-droid <zelenimiko@gmail.com>
2026-06-16 23:25:45 +0200

    Inicijalni commit - frontend i backend osnova

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import dns from 'node:dns/promises';
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+
+import express from 'express';
+import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+import products from './data/products.js';
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.listen(port, () =>
+    console.log(`Server is running on port ${port}`)
+);
