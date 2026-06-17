82bea0a6662f34a486b87a280fa9d8a6bf311245
zelenimiko-droid <zelenimiko@gmail.com>
2026-06-17 23:09:00 +0200

    Dodato 25 proizvoda u bazu, povezan backend sa MongoDB

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ import cors from 'cors'
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
 import userRoutes from './routes/userRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 
@@ -22,18 +21,9 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
-
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
 app.use('/api/users', userRoutes)
+app.use('/api/products', productRoutes)
 
 app.listen(port, () =>
     console.log(`Server is running on port ${port}`)
 );
-app.use('/api/products', productRoutes)
