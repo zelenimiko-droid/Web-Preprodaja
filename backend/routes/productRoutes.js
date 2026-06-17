226b7c14fef2d0e4af40317755440c6765c40597
zelenimiko-droid <zelenimiko@gmail.com>
2026-06-17 23:15:30 +0200

    Dodato filtriranje po kategorijama

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,17 +3,19 @@ import Product from '../models/productModel.js'
 
 const router = express.Router()
 
-// Svi proizvodi sa pretragom
 router.get('/', async (req, res) => {
     const keyword = req.query.keyword
         ? { name: { $regex: req.query.keyword, $options: 'i' } }
         : {}
 
-    const products = await Product.find({ ...keyword })
+    const category = req.query.category
+        ? { category: req.query.category }
+        : {}
+
+    const products = await Product.find({ ...keyword, ...category })
     res.json(products)
 })
 
-// Jedan proizvod
 router.get('/:id', async (req, res) => {
     const product = await Product.findById(req.params.id)
     if (product) {
