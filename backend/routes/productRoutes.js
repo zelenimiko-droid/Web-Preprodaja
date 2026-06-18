dc84ba8e775dae8b82e363487336501233c2196d
zelenimiko-droid <zelenimiko@gmail.com>
2026-06-18 22:11:28 +0200

    Admin proizvodi povezani sa bazom, popravljen dizajn

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,7 @@ import Product from '../models/productModel.js'
 
 const router = express.Router()
 
+// Svi proizvodi sa pretragom i filterom
 router.get('/', async (req, res) => {
     const keyword = req.query.keyword
         ? { name: { $regex: req.query.keyword, $options: 'i' } }
@@ -16,10 +17,22 @@ router.get('/', async (req, res) => {
     res.json(products)
 })
 
+// Jedan proizvod
 router.get('/:id', async (req, res) => {
     const product = await Product.findById(req.params.id)
     if (product) {
         res.json(product)
+    } else {
+        res.status(404).json({ message: 'Proizvod nije pronađen' })
+    }
+})
+
+// Brisanje proizvoda (admin)
+router.delete('/:id', async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        await Product.deleteOne({ _id: product._id })
+        res.json({ message: 'Proizvod obrisan' })
     } else {
         res.status(404).json({ message: 'Proizvod nije pronađen' })
     }
